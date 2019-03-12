66e2c3c199fd66159ada488a8ce579f6ceef9ff7
Duponin <pwet@dupon.in>
2019-03-12 11:55:04 +0100

    Support interface graphique + lancement auto

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <QCoreApplication>
 #include "ccomxbee.h"
+#include "mainwindow.h"
+#include <QCoreApplication>
+#include <QApplication>
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
 
     cComXbee comXbee;
 
+    MainWindow w;
+    w.show();
     return a.exec();
 }
