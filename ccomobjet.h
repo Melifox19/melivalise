2c5c3541a96a7ffc503730f5dcf88fead160dec0
Duponin <pwet@dupon.in>
2019-03-19 14:55:04 +0100

    Belle présenation des méthodes

--- ccomobjet.h
+++ ccomobjet.h
@@ -9,24 +9,24 @@ class cComObjet : public QObject
 public:
     cComObjet(QObject *parent = nullptr);
 
-    void setNumero(int numero);
-    int getNumero();
+    void setNumero               (int numero);
+    int  getNumero               ();
 
-    void setPoids(int poids);
-    int getPoids();
+    void setPoids                (int poids);
+    int  getPoids                ();
 
-    void setBatterie(int batterie);
-    int getBatterie();
+    void setBatterie             (int batterie);
+    int  getBatterie             ();
 
-    void setInterieurTemperature(int temperature);
-    int getInterieurTemperature();
-    void setInterieurHygrometrie(int hygrometrie);
-    int getInterieurHygrometrie();
+    void setInterieurTemperature (int temperature);
+    int  getInterieurTemperature ();
+    void setInterieurHygrometrie (int hygrometrie);
+    int  getInterieurHygrometrie ();
 
-    void setExterieurTemperature(int tempature);
-    int getExterieurTemperature();
-    void setExterieurHygrometrie(int hygrometrie);
-    int getExterieurHygrometrie();
+    void setExterieurTemperature (int tempature);
+    int  getExterieurTemperature ();
+    void setExterieurHygrometrie (int hygrometrie);
+    int  getExterieurHygrometrie ();
 };
 
 #endif // COMOBJET_H
