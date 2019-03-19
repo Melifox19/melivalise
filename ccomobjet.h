a1b7b6104338b79568d33da16e52e8d18f970f3a
Duponin <pwet@dupon.in>
2019-03-19 14:54:26 +0100

    Définition des méthodes

--- ccomobjet.h
+++ ccomobjet.h
@@ -8,6 +8,25 @@ class cComObjet : public QObject
     Q_OBJECT
 public:
     cComObjet(QObject *parent = nullptr);
+
+    void setNumero(int numero);
+    int getNumero();
+
+    void setPoids(int poids);
+    int getPoids();
+
+    void setBatterie(int batterie);
+    int getBatterie();
+
+    void setInterieurTemperature(int temperature);
+    int getInterieurTemperature();
+    void setInterieurHygrometrie(int hygrometrie);
+    int getInterieurHygrometrie();
+
+    void setExterieurTemperature(int tempature);
+    int getExterieurTemperature();
+    void setExterieurHygrometrie(int hygrometrie);
+    int getExterieurHygrometrie();
 };
 
 #endif // COMOBJET_H
