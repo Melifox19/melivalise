10f06192f404c827575b21a49a3cc1475746cbaf
Duponin <pwet@dupon.in>
2019-03-21 09:20:05 +0100

    Ajout variable avec valeur par défaut + fix typo

--- ccomobjet.h
+++ ccomobjet.h
@@ -23,10 +23,19 @@ public:
     void setInterieurHygrometrie (int hygrometrie);
     int  getInterieurHygrometrie ();
 
-    void setExterieurTemperature (int tempature);
+    void setExterieurTemperature (int temperature);
     int  getExterieurTemperature ();
     void setExterieurHygrometrie (int hygrometrie);
     int  getExterieurHygrometrie ();
+
+private:
+    int numero               = 1;
+    int poids                = 85;
+    int batterie             = 90;
+    int interieurTemperature = 20;
+    int interieurHygrometrie = 90;
+    int exterieurTemperature = 25;
+    int exterieurHygrometrie = 90;
 };
 
 #endif // COMOBJET_H
