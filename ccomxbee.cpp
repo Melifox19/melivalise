bfa7987cc034ddb536d690b6a439bc7236731131
Duponin <pwet@dupon.in>
2019-03-26 11:41:18 +0100

    Correction de la regex de validation de requête

--- ccomxbee.cpp
+++ ccomxbee.cpp
@@ -13,7 +13,7 @@ cComXbee::cComXbee(QObject *parent) : QObject(parent)
     serialPort.setDataBits(QSerialPort::Data8);
     serialPort.setPortName("/dev/ttyUSB0");
 
-    requestRE.setPattern("^\\?[1-8];");
+    requestRE.setPattern("^\\?[0-7];");
     if (requestRE.isValid())
     {
         qDebug() << "Regex valide";
